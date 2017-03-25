0e5a9b3006819ee84fca1cbcfc43fc243bd66e71
arsha14 <ansariaarsh14@gmail.com>
2017-03-25 20:00:38 +0530

    generation of the keywords of output

---
+++ filter.py
@@ -0,0 +1,27 @@
+import chat_get
+
+
+c = return_output_text()
+words = c.split()
+def belong(word, listOfWords):
+	a = False;
+	for i in range(len(listOfWords)):
+		if (word == listOfWords[i]):
+			a = True;
+			break;
+	
+	return a; 
+def remove_words(input, standard_words):
+	modified_words = []
+	for i in range(len(input)):
+		if not belong(input[i], common_words):
+			modified_words.append(input[i])
+	return modified_words
+
+common_words = ["the", "are", "is", "be" , "to" , "of" , "and" , "a" , "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about",
+"who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know",  "take",  "into", "year", "your", "some", "could", "them", "see", "other", "than", "then", "now", "only", "its" , "over", "also", "use", "two", "how", "our", "first", "well", "way", "even", "new", "want",
+ "because", "any", "these" ,"give", "day" ,"most" ,"us"]
+
+def gif_text(c) :
+	s = remove_words(c, common_words)
+	def s	
