b33ea76148bcf48b8b97f9e984a0805de52b244e
Alok Kumar Bishoyi <kurdleak.alok@gmail.com>
2017-03-25 15:54:26 +0530

    Added mechanised.
    Need options to return text.

--- chat_get.py
+++ chat_get.py
@@ -1,18 +1,19 @@
-import urllib
-import urllib2
-
-url = 'http://alice.pandorabots.com/'
-values = {'input' : 'Hello!'}
 
 
-data = urllib.urlencode(values)
-req = urllib2.Request(url, data)
-print " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-values = {'input' : 'How are you?'}
-data = urllib.urlencode(values)
-new_req = urllib2.Request(req, data)
+import mechanize
+br = mechanize.Browser()
+br.set_handle_robots( False )
+br.open("http://elbot_e.csoica.artificial-solutions.com/cgi-bin/elbot.cgi")
 
+# User-Agent (this is cheating, ok?)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-response = urllib2.urlopen(new_req)
-the_page = response.read()
-print the_page
+#br.form = list(br.forms())[0]
+#print br.form
+for form in br.forms():
+    print form
+
+br.select_form(nr = 0)
+br.form['ENTRY'] = 'Hello !'
+br.submit()
+br.response.read()
