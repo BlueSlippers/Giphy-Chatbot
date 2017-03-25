4441a894d0136d07bb3597e4d91a9bb9ca46cdda
arsha <ansariaarsh14@gmail.com>
2017-03-25 15:09:56 +0530

    Added giphy.py( added rand ) gif url added commenting and needed corrections done

--- giphy.py
+++ giphy.py
@@ -1,17 +1,17 @@
-import giphypop
+import giphypop      #giphypop is a class that accesses the giphy api
 import random
 
 
-def getgifurl(gifkeyword):
+def getgifurl(gifkeyword):      #takes in the key word and returns the url of the related gif
     i = random.randint(0,5)
     g = giphypop.Giphy()
-    results=[x for x in g.search(gifkeyword)]
-    t = str(results[i])
+    results=[x for x in g.search(gifkeyword)]     #creates a list of
+    t = str(results[i])       #taking a random gif in the given list
     n = t.find('-')
-    while True:
+    while True:                 #finding the id of gif
         if t.find('-',n+1)==-1:
             break
         n = t.find('-',n+1)
     length = len(t)
-    url = "https://media.giphy.com/media/"+t[:n-length+1]+"/giphy.gif"
+    url = "https://media.giphy.com/media/"+t[n-length+1:]+"/giphy.gif"
     return url
