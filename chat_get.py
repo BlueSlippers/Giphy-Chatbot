57bd0bd98af6e2c5e5bcb7e0c53825b4c5f372fb
Alok Kumar Bishoyi <kurdleak.alok@gmail.com>
2017-03-25 17:58:20 +0530

    Get new_url from chat_get.py
    Still left to process the words.

--- chat_get.py
+++ chat_get.py
@@ -1,24 +1,26 @@
 
+
 import mechanize
-br = mechanize.Browser()
-br.set_handle_robots( False )
-br.open("http://elbot_e.csoica.artificial-solutions.com/cgi-bin/elbot.cgi")
+import cache
 
-# User-Agent (this is cheating, ok?)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+def return_output_html(string):
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
 
-#br.form = list(br.forms())[0]
-#print br.form
-for form in br.forms():
-    print form
+    br.open(cache.base_url)
 
-br.select_form(nr = 0)
-br.form['ENTRY'] = 'Hello !'
+    # User-Agent (this is cheating, ok?)
+    br.addheaders = [('User-agent',
+                      'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-response =  br.submit()
-new_url = response.geturl()
-x = br.open(new_url)
+    br.select_form(nr=0)
+    br.form['ENTRY'] = string
 
-print x.read()
+    response = br.submit()
+    new_url = response.geturl()
+
+    cache.base_url = new_url
+
+
 
 
