e8045c8c76001e581378f94b26c800446730fe81
arsha <ansariaarsh14@gmail.com>
2017-03-25 14:55:31 +0530

    Added giphy.py( added rand ) gif url added

--- giphy.py
+++ giphy.py
@@ -3,9 +3,15 @@ import random
 
 
 def getgifurl(gifkeyword):
-    i = random.randint(0,5+)
+    i = random.randint(0,5)
     g = giphypop.Giphy()
     results=[x for x in g.search(gifkeyword)]
-    s = str(results[i])[-13:]
-
-    return results[i]
+    t = str(results[i])
+    n = t.find('-')
+    while True:
+        if t.find('-',n+1)==-1:
+            break
+        n = t.find('-',n+1)
+    length = len(t)
+    url = "https://media.giphy.com/media/"+t[:n-length+1]+"/giphy.gif"
+    return url
