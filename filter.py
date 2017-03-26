6a9db142fc57aa0d4628179b916a228c83f731b0
Alok Kumar Bishoyi <kurdleak.alok@gmail.com>
2017-03-26 11:40:54 +0530

    Updated and modified filter

--- filter.py
+++ filter.py
@@ -1,16 +1,15 @@
 import chat_get
+common_words = ["the", "are", "is", "be" , "to" , "of" , "and" , "a" , "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about",
+"who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know",  "take",  "into", "year", "your", "some", "could", "them", "see", "other", "than", "then", "now", "only", "its" , "over", "also", "use", "two", "how", "our", "first", "well", "way", "even", "new", "want",
+ "because", "any", "these" ,"give", "day" ,"most" ,"us"]
 
 
-c = return_output_text()
-words = c.split()
 def belong(word, listOfWords):
 	a = False;
 	for i in range(len(listOfWords)):
 		if (word == listOfWords[i]):
 			a = True;
 			break;
-	
-	return a; 
 def remove_words(input, standard_words):
 	modified_words = []
 	for i in range(len(input)):
@@ -18,10 +17,12 @@ def remove_words(input, standard_words):
 			modified_words.append(input[i])
 	return modified_words
 
-common_words = ["the", "are", "is", "be" , "to" , "of" , "and" , "a" , "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about",
-"who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know",  "take",  "into", "year", "your", "some", "could", "them", "see", "other", "than", "then", "now", "only", "its" , "over", "also", "use", "two", "how", "our", "first", "well", "way", "even", "new", "want",
- "because", "any", "these" ,"give", "day" ,"most" ,"us"]
+def split_string(s): #To be called by main
 
-def gif_text(c) :
-	s = remove_words(c, common_words)
-	def s	
+	words = s.split()
+	a = remove_words(words, common_words)
+
+	temp_string = ""
+	for x in a:
+		temp_string+=str(x)+" "
+	return temp_string
