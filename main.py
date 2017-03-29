f49ff17e87148c510425bb62cd498995a023d9e4
Alok Kumar Bishoyi <kurdleak.alok@gmail.com>
2017-03-29 06:19:37 +0530

    main.py created.
    Chatbox gives decent replies.

---
+++ main.py
@@ -0,0 +1,61 @@
+from Tkinter import *
+import mechanize
+import re
+
+###Mechanize set-up
+url = "http://elbot_e.csoica.artificial-solutions.com/cgi-bin/elbot.cgi"
+br = mechanize.Browser()
+br.set_handle_robots(False)
+
+#br.open(cache.base_url)
+br.open( url )
+# User-Agent (this is cheating, ok?)
+br.addheaders = [('User-agent',
+                  'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+###Finised setting of virtual browser
+
+###Setting of Regex values
+regex = '<style><.+?></style>'
+regex1 = '(?<=-->)(.*)(?=<!)'
+pattern = re.compile(regex1)
+###Done
+
+def Return_output( string ):
+    br.select_form(nr=0)
+    br.form['ENTRY'] = string
+    response = br.submit()
+    text = response.read()
+    reply = re.findall(pattern, text)
+    return reply
+
+
+window = Tk()
+
+input_user = StringVar()
+input_field = Entry(window, text=input_user)
+input_field.pack(side=BOTTOM, fill=X)
+
+scroll = Scrollbar(window)
+scroll.pack(side=RIGHT, fill=BOTH)
+
+def enter_pressed(event):
+
+    input_get = input_field.get()
+    print(input_get)
+    label = Label(frame, text=input_get)
+    input_user.set('')
+    label.pack()
+
+    output_label = Label(frame, text =Return_output(input_get))
+    output_label.pack()
+
+    return "break"
+
+frame = Frame(window, width=300, height=300)
+frame.pack_propagate(False) # prevent frame to resize to the labels size
+input_field.bind("<Return>", enter_pressed)
+frame.pack()
+
+
+
+window.mainloop()
